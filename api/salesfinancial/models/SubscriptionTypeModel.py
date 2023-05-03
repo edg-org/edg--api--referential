4d789590878105c6cf241caa06b6cd57f3dd8530
Ousmane <ousmane.diakite@ext.adeo.com>
2023-05-03 13:23:42 +0200

    feat(update): correction of bugs and adding the input for the test

--- api/salesfinancial/models/SubscriptionTypeModel.py
+++ api/salesfinancial/models/SubscriptionTypeModel.py
@@ -9,11 +9,11 @@ class SubscriptionTypeModel(EntityMeta):
 
     id = Column(TINYINT(unsigned=True), primary_key=True, index=True)
     code = Column(TINYINT(unsigned=True), index=True, unique=True, nullable=False)
-    name = Column(String(20), index=True, unique=True, nullable=False)
+    name = Column(String(100), index=True, unique=True, nullable=False)
     infos = Column(JSON, nullable=False)
     pricing = Column(JSON, nullable=False)
     dunning = Column(JSON, nullable=False)
-    tracking_id = Column(TINYINT(unsigned=True), ForeignKey("tracking_types.id"), nullable=False)
+    tracking_type_id = Column(TINYINT(unsigned=True), ForeignKey("tracking_types.id"), nullable=False)
     power_mode_id = Column(TINYINT(unsigned=True), ForeignKey("power_modes.id"), nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=datetime.utcnow().isoformat(), nullable=False)
     updated_at = Column(DateTime(timezone=True), onupdate=datetime.utcnow().isoformat(), nullable=True)
